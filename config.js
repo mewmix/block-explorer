93c1ba5a155bae9df1a130046bc098b3802d04eb
chubbykat <42463809+mewmix@users.noreply.github.com>
2018-10-30 00:30:46 -0700

    Update config.js

--- config.js
+++ config.js
@@ -7,21 +7,7 @@ var refreshDelay = 30000;
 // pools stats by MainCoins
 var networkStat = {
  "TNGU": [
-<<<<<<< HEAD
 	
-=======
-	["INSTANCE1", "http://z-pool.com:8117"],
-	["INSTANCE2", "http://eu.turtlepool.space:8117"],
-	["INSTANCE3", "http://us.turtlepool.space:8117"],
-	["INSTANCE4", "http://hk.turtlepool.space:8117"],
-	["turtlecoinpool.ddns.net", "http://turtlecoinpool.ddns.net:8127"],
-	["trtl.mine2gether.com", "https://trtl.mine2gether.com/api"],
-	["trtl.heigh-ho.funkypenguin.co.nz", "https://api.trtl.heigh-ho.funkypenguin.co.nz"],
-	["trtl.radicaldelta.org", "http://trtl.radicaldelta.org:8117"],
-	["turtle.atpool.party", "http://turtle-eu.atpool.party:8117"],
-	["ny.minetrtl.us", "http://ny.minetrtl.us:8117"],
-	["xk.is", "https://xk.is/api"]
->>>>>>> 36af0278ad50a65be7db47ede6b3a2307e4d6b31
  ]
 };
 
