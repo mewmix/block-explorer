98d38ebade7c8f6e6306d69e7279686b0ee3634e
root <root@instance-5.us-east1-b.c.erudite-visitor-220901.internal>
2018-10-30 06:37:18 +0000

    First commit

---
+++ config.js
@@ -0,0 +1,18 @@
+var api = 'http://104.196.174.142:11898';
+var donationAddress = "";
+var blockTargetInterval = 30;
+var coinUnits = 100;
+var symbol = 'TNGU';
+var refreshDelay = 30000;
+// pools stats by MainCoins
+var networkStat = {
+ "TNGU": [
+	
+ ]
+};
+
+var networkStat2 = {
+    "TNGU": [
+	[""]
+ ]
+};
---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+return array(
+        'api' => 'http://104.196.174.142:11898',
+        'blockTargetInterval' => 30,
+        'coinUnits' => 100
+);
